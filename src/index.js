73809d2a7c8897471219800652e1f829312a1ee1
scarter31191 <scarter31191@gmaile.com>
2021-09-18 09:51:23 -0400

    created a way to take turns between x and o

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import './index.css';
 function Square(props) {
   
     return ( //the component takes in the props its passed 
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value} 
+      <button className="square" onClick={() => props.onClick()}> 
+        {props.value} 
       </button>
     );
 }
@@ -17,14 +17,17 @@ class Board extends React.Component {
   constructor(props){
     super(props);
       this.state = {
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        xIsNext: true
       }
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice() //.slice() is immutable it will not change the data 
-      squares[i] = 'X';
-      this.setState({squares: squares})
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
     }
 
 
@@ -36,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
